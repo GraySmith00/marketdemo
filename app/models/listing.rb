50ca93e6fd521a1d5cecd639f56dd941ec5b58b9
Gray Smith <gray.smith00@gmail.com>
2015-09-03 20:29:54 +0000

    associate listings with users and add user_id to listings table

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,5 @@
 class Listing < ActiveRecord::Base
+  belongs_to :user
   
   has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
