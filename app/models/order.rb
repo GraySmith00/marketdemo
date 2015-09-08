4a50e97767e84135e81077220cdd9ca9d4d6771b
Gray Smith <gray.smith00@gmail.com>
2015-09-08 18:22:25 +0000

    enable users to place orders and add associations between users listings and orders

---
+++ app/models/order.rb
@@ -0,0 +1,7 @@
+class Order < ActiveRecord::Base
+  belongs_to :listing
+  belongs_to :buyer, class_name: "User"
+  belongs_to :seller, class_name: "User"
+  
+  validates :address, :city, :state, presence: :true
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  resources :listings
+  resources :listings do
+    resources :orders
+  end
   
   get "pages/about"
   get "pages/contact"
